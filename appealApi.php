10af37a7194e90210c72a325e804a7ee50269661
n0099 <n@n0099.net>
2021-10-14 05:02:32 +0800

    fix not checking is requesting fid matches the user configured forum in appealApi.php by remember fid in a tmp file when first visited

--- appealApi.php
+++ appealApi.php
@@ -1,4 +1,5 @@
 <?php
+require 'core.php';
 
 // also in appealSW.js
 $allowedApiEndpoints = [
@@ -10,13 +11,20 @@ $allowedApiEndpoints = [
     'appeal/next',
     'appeal/tpl'
 ];
+$fidFilename = './fid.' . KW_RAW . '.tmp';
 
-if (empty($_GET['url'] ?? null) && !in_array($_GET['url'], $allowedApiEndpoints, true)) {
+if (file_exists($fidFilename)) {
+    $fid = (int)file_get_contents($fidFilename);
+} else {
+    $fid = json_decode(cget('http://tieba.baidu.com/mg/f/getFrsData?kw=' . KW, COOKIE))->data->forum->id;
+    file_put_contents($fidFilename, $fid);
+}
+
+if ((int)$_GET['forum_id'] !== $fid
+    || (empty($_GET['url'] ?? null) && !in_array($_GET['url'], $allowedApiEndpoints, true))) {
     http_response_code(400);
     die();
-};
-
-require 'core.php';
+}
 
 $url = $_GET['url'];
 unset($_GET['url']);
@@ -26,29 +34,38 @@ if (HIDE) {
 $res = cget('http://tieba.baidu.com/bawu2/' . $url . '?' . http_build_query($_GET), COOKIE);
 if ($res === 'err') {
     http_response_code(502);
-    die();
+    exit();
+}
+
+$isHideDeletedPostContent = HIDE_DELETED_POST_CONTENT && $url === 'postappeal/detailAppeal';
+if (HIDE
+    || ($isHideDeletedPostContent)) {
+    $res = json_decode($res, true);
+    if (HIDE) {
+        $hideOperators = function (&$v) use (&$hideOperators) {
+            if (is_array($v)) {
+                $idFieldsToBeHide = ['del_uid', 'op_uid'];
+                // operate_man only appears in appealRecordList[0] of appeal/list endpoint as the operator who baned that user
+                $usernameFieldsToBeHide = ['del_uname', 'op_uname', 'operate_man'];
+                array_walk($v, function (&$v, $k) use ($idFieldsToBeHide, $usernameFieldsToBeHide) {
+                    if (in_array($k, $idFieldsToBeHide, true)) {
+                        $v = 0;
+                    }
+                    if (in_array($k, $usernameFieldsToBeHide, true)) {
+                        $v = 'Hidden';
+                    }
+                });
+                array_walk($v, $hideOperators); // recursive
+            }
+        };
+        array_walk($res, $hideOperators);
+    }
+    if ($isHideDeletedPostContent) {
+        $res['data']['content'] = '';
+        $res['data']['post_img'] = $res['data']['post_vedio'] = $res['data']['post_audio'] = [];
+    }
+    $res = json_encode($res);
 }
 
 header('Content-Type: application/json');
-if (HIDE) {
-    $res = json_decode($res, true);
-    $hideOperators = function (&$v) use (&$hideOperators) {
-        if (is_array($v)) {
-            $idFieldsToBeHide = ['del_uid', 'op_uid'];
-            $usernameFieldsToBeHide = ['del_uname', 'op_uname', 'operate_man']; // operate_man only appears in appealRecordList[0] of appeal/list endpoint as the operator who baned that user
-            array_walk($v, function (&$v, $k) use ($idFieldsToBeHide, $usernameFieldsToBeHide) {
-                if (in_array($k, $idFieldsToBeHide, true)) {
-                    $v = 0;
-                }
-                if (in_array($k, $usernameFieldsToBeHide, true)) {
-                    $v = 'Hidden';
-                }
-            });
-            array_walk($v, $hideOperators); // recursive
-        }
-    };
-    array_walk($res, $hideOperators);
-    echo json_encode($res);
-} else {
-    echo $res;
-}
+echo $res;
